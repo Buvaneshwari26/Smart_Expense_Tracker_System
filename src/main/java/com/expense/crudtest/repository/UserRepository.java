376da4fe428fc37da1d556c6cbdcbdffc12838c7
Buvaneshwari Periyasamy <2k23cse021@kiot.ac.in>
2026-06-14 20:29:56 +0530

    Removed old crudtest project and kept tracker project

--- src/main/java/com/expense/crudtest/repository/UserRepository.java
+++
@@ -1,7 +0,0 @@
-package com.expense.crudtest.repository;
-
-import com.expense.crudtest.entity.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserRepository extends JpaRepository<User, Integer> {
-}
